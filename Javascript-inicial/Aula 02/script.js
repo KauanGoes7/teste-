057fec092c58d7cf3b50a03a54691d0c28ea68b5
KauanGoes7 <kauangoes3443@gmail.com>
2025-02-18 17:13:13 -0300

    subundo meu cod

---
+++ Javascript-inicial/Aula 02/script.js
@@ -0,0 +1,30 @@
+// const  primeiro = 10 
+// const  segundo = 2 
+// const  terceiro = 5
+
+// function soma(){
+//     var a = 5
+//     var b = 2 
+//     var res = a + b
+//     return " resulrado: "(segundo + terceiro)
+// }
+
+// function printText(){
+//     console.log("esta eh uma função de soma")
+// }
+// console.log(soma())
+//         function somarcomparametros(batata,n2,n3){
+//             return batata + n2 + n3
+//         }
+//         console.log(somarcomparametros(2,2,2))
+
+const somarcomparametros = () => n1 + n2 + n3
+
+console.log(somarcomparametros(2,2,2))
+
+const teste = () => "Texto Qualquer"
+
+console.log(teste())
+
+
+
